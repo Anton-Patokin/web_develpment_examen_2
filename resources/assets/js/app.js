1fdada8f7aa8059c36144632b0cff7d8165e1413
Anton-Patokin <patokin.anton@gmail.com>
2017-01-12 23:39:13 +0100

    update routes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,7 +7,7 @@ require('./components/filter.js');
 (function ($) {
     $(document).ready(function () {
         var getUrl = window.location;
-        var baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1] + '/public/';
+        var baseUrl = '/';
 
 
 
